978e8ea9c9b0dce84b5f969db6b5f72d4da03094
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-06-23 12:19:39 +0600

    plantdetails single page setup

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -31,6 +31,9 @@ export const router = createBrowserRouter([
       {
         path: "/plant/:id",
         element: <PlantDetails />,
+        loader: ({ params }) =>
+          fetch(`${import.meta.env.VITE_API_URL}/plant/${params.id}`),
+        HydrateFallback: LoadingSpinner,
       },
     ],
   },
